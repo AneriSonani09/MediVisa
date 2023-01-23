496f8b2c3e716a14aa4ead67d801450eae1c3335
Parangi Rathod <82773925+Parangi-27@users.noreply.github.com>
2023-01-23 22:53:02 +0530

    Registration & Login

---
+++ server/models/user.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const Joi = require('joi'); 
+const passwordComplexity = require("joi-password-complexity");
+
+const userSchema = new mongoose.Schema({
+    firstName: {type: String, reuired: true},
+    lastName: {type: String, reuired: true},
+    email: {type: String, reuired: true},
+    password: {type: String, reuired: true},
+});
+
+userSchema.methods.generateAuthToken = function(){
+    const token = jwt.sign({_id: this._id}, process.env.JWTPRIVATEKEY, {expiresIn: "7d"});
+    return token
+};
+
+const User = mongoose.model("user", userSchema);
+
+const validate = (data) => {
+    const schema = Joi.object({
+        firstName: Joi.string().required().label("First Name"),
+        lastName: Joi.string().required().label("Last Name"),
+        email: Joi.string().email().required().label("Email"),
+        password: passwordComplexity().required().label("Password")
+    });
+    return schema.validate(data);
+};
+
+module.exports = {User, validate};
