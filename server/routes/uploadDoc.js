126a1e2ac25b32ebe43b80cc2baa01e487473311
Parangi Rathod <82773925+Parangi-27@users.noreply.github.com>
2023-04-11 20:46:58 +0530

    Update uploadDoc.js

--- server/routes/uploadDoc.js
+++ server/routes/uploadDoc.js
@@ -1,23 +1,33 @@
 const router = require("express").Router();
-const uploadmodel = require('../models/uploads')
+const uploadmodel = require("../models/uploads");
 
 router.post("/", async (req, res) => {
-    console.log("/*/*/")
-    console.log(req.body)
-    console.log(req.body.User)
-    console.log(req.body.Attach)
-    try {
-        const upload = new uploadmodel.uploads(req.body);
-        await upload.save();
-        const uploadDoc = req.body;
-        console.log(uploadDoc);
-        const obj = {
-            uploadDoc: upload,
-      };
-        return res.status(200).json(obj);
-    } catch (err) {
-        res.status(500).json(err);
-    }
+  console.log("/*/*/");
+  console.log(req.body);
+  console.log(req.body.User);
+  console.log(req.body.Attach);
+  const upload = new uploadmodel(req.body);
+  upload.save((err, res) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send("Failed to insert booking");
+    } else {
+      //console.log(result);
+      res.status(200).send(result);
+    }
+  });
+  //     try {
+  //         const upload = new uploadmodel.uploads(req.body);
+  //         await upload.save();
+  //         const uploadDoc = req.body;
+  //         console.log(uploadDoc);
+  //         const obj = {
+  //             uploadDoc: upload,
+  //       };
+  //         return res.status(200).json(obj);
+  //     } catch (err) {
+  //         res.status(500).json(err);
+  //     }
 });
 
 module.exports = router;
